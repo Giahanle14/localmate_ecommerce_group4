2809ae09da2f725b188293d94b6491f26c0b1437
ngoctran31231024952 <ngoctran.31231024952@st.ueh.edu.vn>
2026-05-29 03:16:11 +0700

    Ngoc_TrangYeuThich(2)

--- app/controllers/tourdetailcontroller.php
+++ app/controllers/tourdetailcontroller.php
@@ -26,18 +26,26 @@ class TourDetailController {
         // 1. Tính năng yêu thích tạm thời tắt để chờ thành viên khác ghép code
         $isFavorite = false;
 
-        // 2. XỬ LÝ BREADCRUMB ĐỘNG (Trang chủ > Chuyến đi của tôi > Chi tiết tour)
+        // 2. XỬ LÝ BREADCRUMB ĐỘNG (Trang chủ > Chuyến đi của tôi / Yêu thích / Tour > Chi tiết tour)
         $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+        $from = isset($_GET['from']) ? $_GET['from'] : ''; // Ưu tiên bắt tham số from trên URL nếu có
+        
         $breadcrumb = [['name' => 'Trang chủ', 'url' => 'index.php?controller=home']];
         
-        if (strpos($referer, 'controller=mytrip') !== false) {
+        if ($from === 'favorite' || strpos($referer, 'controller=favorite') !== false) {
+            $breadcrumb[] = ['name' => 'Yêu thích', 'url' => 'index.php?controller=favorite'];
+        } 
+        else if (strpos($referer, 'controller=mytrip') !== false) {
             $breadcrumb[] = ['name' => 'Chuyến đi của tôi', 'url' => 'index.php?controller=mytrip'];
-        } else if (strpos($referer, 'controller=tour') !== false && strpos($referer, 'action=detail') === false) {
+        } 
+        else if (strpos($referer, 'controller=tour') !== false && strpos($referer, 'action=detail') === false) {
             $breadcrumb[] = ['name' => 'Tour', 'url' => 'index.php?controller=tour'];
-        } else {
+        } 
+        else {
             // Mặc định nếu không rõ từ đâu tới
             $breadcrumb[] = ['name' => 'Tour', 'url' => 'index.php?controller=tour'];
         }
+        
         $breadcrumb[] = ['name' => 'Chi tiết tour', 'url' => '#'];
 
         // Gọi View hiển thị giao diện
